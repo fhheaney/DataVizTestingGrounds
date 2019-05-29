c07f5226bb5c4b6dbfe5f5f0a4ef21b762f3dfa2
Fiona Heaney <fhheaney@wpi.edu>
2019-05-29 17:47:46 -0400

    zoomable_icicle

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Reporting Team Data Visualization Tracer Bullets
+# DataVizTestingGrounds
 
 <li><a href="https://fhheaney.github.io/DataVizTestingGrounds/radial_sunburst/index.html">Static Radial Sunburst</a></li>
 <li><a href="https://fhheaney.github.io/DataVizTestingGrounds/zoomable_sunburst/index.html">Zoomable Radial Sunburst</a></li>
+<li><a href="https://fhheaney.github.io/DataVizTestingGrounds/zoomable_icicle/index.html">Zoomable Icicle</a></li>
 
 Static Sunburst design from <a href="https://bl.ocks.org/kerryrodden/7090426">kerryrodden</a>
 <br>
 Zoomable Sunburst design from <a href="https://bl.ocks.org/vasturiano/raw/12da9071095fbd4df434e60d52d2d58d/">vasturiano</a>
+<br>
+Zoomable Icicle design from <a href="https://embed.plnkr.co/plunk/XqOyFU">mbostock</a>
---
+++ zoomable_icicle/script.js
@@ -0,0 +1,156 @@
+// URL: https://observablehq.com/@d3/zoomable-icicle
+// Title: Zoomable Icicle
+// Author: D3 (@d3)
+// Version: 243
+// Runtime version: 1
+
+const m0 = {
+  id: "349116b280ed2fdf@243",
+  variables: [
+    {
+      inputs: ["md"],
+      value: (function(md){return(
+md`# Zoomable Icicle`)})
+    },
+    {
+      name: "chart",
+      inputs: ["partition","data","d3","DOM","width","height","color","format"],
+      value: (function(partition,data,d3,DOM,width,height,color,format)
+{
+  const root = partition(data);
+  let focus = root;
+
+  const svg = d3.select(DOM.svg(width, height))
+      .style("width", "100%")
+      .style("height", "900px")
+      .style("overflow", "hidden")
+      .style("font", "15px sans-serif");
+
+  const cell = svg
+    .selectAll("g")
+    .data(root.descendants())
+    .join("g")
+      .attr("transform", d => `translate(${d.y0},${d.x0})`);
+
+  const rect = cell.append("rect")
+      .attr("width", d => d.y1 - d.y0 - 1)
+      .attr("height", d => rectHeight(d))
+      .attr("fill-opacity", 0.6)
+      .attr("fill", d => {
+        if (!d.depth) return "#ccc";
+        while (d.depth > 1) d = d.parent;
+        return d.data.color;
+      })
+      .style("cursor", "pointer")
+      .on("click", clicked);
+
+  const text = cell.append("text")
+      .style("user-select", "none")
+      .attr("pointer-events", "none")
+      .attr("x", 4)
+      .attr("y", 13)
+      .attr("fill-opacity", d => +labelVisible(d));
+
+  text.append("tspan")
+      .text(d => d.data.name);
+
+  const tspan = text.append("tspan")
+      .attr("fill-opacity", d => labelVisible(d) * 0.7)
+      .text(d => ` ${format(d.value)}`);
+
+  cell.append("title")
+      .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+
+  function clicked(p) {
+    focus = focus === p ? p = p.parent : p;
+
+    root.each(d => d.target = {
+      x0: (d.x0 - p.x0) / (p.x1 - p.x0) * height,
+      x1: (d.x1 - p.x0) / (p.x1 - p.x0) * height,
+      y0: d.y0 - p.y0,
+      y1: d.y1 - p.y0
+    });
+
+    const t = cell.transition().duration(750)
+        .attr("transform", d => `translate(${d.target.y0},${d.target.x0})`);
+
+    rect.transition(t).attr("height", d => rectHeight(d.target));
+    text.transition(t).attr("fill-opacity", d => +labelVisible(d.target));
+    tspan.transition(t).attr("fill-opacity", d => labelVisible(d.target) * 0.7);
+  }
+  
+  function rectHeight(d) {
+    return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+  }
+
+  function labelVisible(d) {
+    return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+  }
+  
+  return svg.node();
+}
+)
+    },
+    {
+      name: "data",
+      inputs: ["d3"],
+      value: (function(d3){return(
+d3.json("./data.json")
+)})
+    },
+    {
+      name: "partition",
+      inputs: ["d3","height","width"],
+      value: (function(d3,height,width){return(
+data => {
+  const root = d3.hierarchy(data)
+      .sum(d => d.value)
+      .sort((a, b) => b.height - a.height || b.value - a.value);
+  return d3.partition()
+      .size([height, (root.height + 1) * width / 3])
+    (root);
+}
+)})
+    },
+    {
+      name: "color",
+      inputs: ["d3","data"],
+      value: (function(d3,data){return(
+d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+)})
+    },
+    {
+      name: "format",
+      inputs: ["d3"],
+      value: (function(d3){return(
+d3.format(",d")
+)})
+    },
+    {
+      name: "width",
+      value: (function(){return(
+975
+)})
+    },
+    {
+      name: "height",
+      value: (function(){return(
+1200
+)})
+    },
+    {
+      name: "d3",
+      inputs: ["require"],
+      value: (function(require){return(
+require("d3@5")
+)})
+    }
+  ]
+};
+
+const notebook = {
+  id: "349116b280ed2fdf@243",
+  modules: [m0]
+};
+
+export default notebook;
